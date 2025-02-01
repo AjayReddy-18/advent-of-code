32fbb7d4b0ade27004cd1d4da9cb76cf35319630
Ajay Reddy <iamajayreddy18@gmail.com>
2025-02-01 17:22:01 +0530

    completed part 2 of day 3

--- 2019/src/day_03/part_1.js
+++ 2019/src/day_03/part_1.js
@@ -29,10 +29,11 @@ const cordinates = (previousCordinates, direction, offset) => {
 };
 
 const lineSegment = (previousCordinates) => (path) => {
-  const { x: x2, y: y2 } = cordinates(previousCordinates, ...sliceAt(path, 1));
-  const { x: x1, y: y1 } = previousCordinates;
-  previousCordinates = { x: x2, y: y2 };
-  return { x1, y1, x2, y2 };
+  const [direction, offset] = sliceAt(path, 1);
+  const { x: x2, y: y2 } = cordinates(previousCordinates, direction, offset);
+  const { x: x1, y: y1, steps } = previousCordinates;
+  previousCordinates = { x: x2, y: y2, steps: steps + +offset };
+  return { x1, y1, x2, y2, steps: previousCordinates.steps };
 };
 
 const [route1, route2] = fetchInput("input/day_03/part_1.txt");
@@ -40,42 +41,33 @@ const [route1, route2] = fetchInput("input/day_03/part_1.txt");
 const wire1 = route1.split(",");
 
 const lines1 = [];
-const lineSegmentToWire1 = lineSegment({ x: 0, y: 0 });
+const lineSegmentToWire1 = lineSegment({ x: 0, y: 0, steps: 0 });
 
 for (const path of wire1) {
-  const { x1, y1, x2, y2 } = lineSegmentToWire1(path);
-  lines1.push(Line.equation(x1, y1, x2, y2));
+  const { x1, y1, x2, y2, steps } = lineSegmentToWire1(path);
+  lines1.push([Line.equation(x1, y1, x2, y2), steps]);
 }
 
 const wire2 = route2.split(",");
 
 const lines2 = [];
-const lineSegmentToWire2 = lineSegment({ x: 0, y: 0 });
+const lineSegmentToWire2 = lineSegment({ x: 0, y: 0, steps: 0 });
 
 for (const path of wire2) {
-  const { x1, y1, x2, y2 } = lineSegmentToWire2(path);
-  lines2.push(Line.equation(x1, y1, x2, y2));
+  const { x1, y1, x2, y2, steps } = lineSegmentToWire2(path);
+  lines2.push([Line.equation(x1, y1, x2, y2), steps]);
 }
 
 const intersections = [];
 
-for (const line1 of lines1) {
-  for (const line2 of lines2) {
+for (const [line1, steps1] of lines1) {
+  for (const [line2, steps2] of lines2) {
     const intersection = line1.intersection(line2);
     if (line1.onSegment(intersection) && line2.onSegment(intersection)) {
-      intersections.push(intersection);
+      const stepsTaken = steps1 + steps2;
+      intersections.push({ ...intersection, stepsTaken });
     }
   }
 }
 
-const distance = ({ x, y }) => Math.sqrt(x * x + y * y);
-
-const closestIntersection = (intersections) => {
-  const distances = {};
-  for (const intersection of intersections) {
-    distances[distance(intersection)] = intersection;
-  }
-  return distances[Math.min(...Object.keys(distances))];
-};
-
-console.log(closestIntersection(intersections));
+console.log(intersections);
